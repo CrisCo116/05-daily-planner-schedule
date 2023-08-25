349811e7dc30195e252aca6f1960e5a1f6142d02
Chris <138630942+CrisCo116@users.noreply.github.com>
2023-08-24 17:55:36 -0700

    added in the save button function and the local storage function.

--- script.js
+++ script.js
@@ -12,7 +12,11 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-
+  $('.container-fluid').on('click', '.saveBtn', function() {
+    var timeBlockId = $(this).closest('.time-block').attr('id');
+    var userInput = $(this).siblings('.description').val();
+    localStorage.setItem(timeBlockId, userInput);
+  });
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -22,15 +26,16 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  
+  $('.time-block').each(function() {
+    var timeBlockId = $(this).attr('id');
+    var storedInput = localStorage.getItem(timeBlockId);
+    if (storedInput) {
+      $(this).find('.description').val(storedInput);
+    }
+  });
   // TODO: Add code to display the current date in the header of the page.
   var dayJsObject = dayjs();
   var dateEl = document.querySelector('#currentDay');
   dateEl.textContent = dayJsObject.format("MMMM DD YYYY hh:mm A");
-
-  function currentDay() {
-    var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
-    timeDisplayEl.text(rightNow);
-  }
 
 });
